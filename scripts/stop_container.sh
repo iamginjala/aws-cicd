877a3174630458f5d86e6616b6576a43bead8665
Harsha Vardhan Reddy Ginjala <153235811+iamginjala@users.noreply.github.com>
2026-01-16 13:53:47 -0500

    Enhance stop_container.sh to stop running containers

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,12 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+ContainerId=$(sudo docker ps -q)
+echo "Container ID: $ContainerId"
+
+if [ ! -z "$ContainerId" ]; then
+    echo "Stopping container $ContainerId"
+    sudo docker rm -f $ContainerId
+else
+    echo "No running containers found"
+fi
